74c29cbe82f1c6cf37bd2c71a3cc93ba171c5c8e
ItSushko14 <ItSushkoAndriy@gmail.com>
2023-04-29 17:25:30 +0300

    fix server.js

--- server.js
+++ server.js
@@ -52,31 +52,6 @@ app.listen(PORT, (err) => {
 	console.log('Server started on PORT ' + PORT);
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD-9zY5NAFS_dTA1dlbZXAwh1DwRlWWIfE",
-  authDomain: "cheff-173de.firebaseapp.com",
-  projectId: "cheff-173de",
-  storageBucket: "cheff-173de.appspot.com",
-  messagingSenderId: "487205944495",
-  appId: "1:487205944495:web:56db277958a946cc05685a",
-  measurementId: "G-PXF2BYHGP2"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
-
-
 
 
 
