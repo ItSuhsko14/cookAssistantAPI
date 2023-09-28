6b4d4ad6bd5c27c4cc62a34a006291a8568a73ab
ItSushko14 <ItSushkoAndriy@gmail.com>
2023-09-28 17:03:54 +0300

    add chefassistant.pp.ua and vercel.com in cors policy

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-	origin: ['https://itsuhsko14.github.io', 'http://localhost:3000'],
+	origin: ['https://itsuhsko14.github.io', 'http://localhost:3000', 'https://chef-assistant-chefapp.vercel.app/', 'https://www.chefassistant.pp.ua/'],
 }))
 
 app.get('/',  (req, res) => {			
